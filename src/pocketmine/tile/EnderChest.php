a69a2d7dbeb6ecf341777d6532ff8ffef5db0be7
TSRlightda <lightda104530@gmail.com>
2017-02-14 18:11:08 +0800

    Add EnderChest??

---
+++ src/pocketmine/tile/EnderChest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace pocketmine\tile;
+
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\IntTag;
+use pocketmine\nbt\tag\StringTag;
+
+class EnderChest extends Spawnable implements Nameable{
+
+	public function getName() : string{
+		return isset($this->namedtag->CustomName) ? $this->namedtag->CustomName->getValue() : "Ender Chest";
+	}
+  
+	public function hasName(){
+		return isset($this->namedtag->CustomName);
+	}
+  
+	public function setName($str){
+		if($str === ""){
+			unset($this->namedtag->CustomName);
+			return;
+		}
+    
+		$this->namedtag->CustomName = new StringTag("CustomName", $str);
+	}
+  
+	public function getSpawnCompound(){
+		$enderchest = new CompoundTag("", [
+			new StringTag("id", Tile::ENDER_CHEST),
+			new IntTag("x", (int) $this->x),
+			new IntTag("y", (int) $this->y),
+			new IntTag("z", (int) $this->z)
+		]);
+    
+		if($this->hasName()){
+			$enderchest->CustomName = $this->namedtag->CustomName;
+		}
+    
+		return $enderchest;
+	}
+  
+}
