3a842d084149eeb38365ac8efe627de1654aa031
TSRlightda <lightda104530@gmail.com>
2017-02-14 18:13:12 +0800

    EnderChest?

---
+++ src/pocketmine/inventory/EnderChestInventory.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace pocketmine\inventory;
+
+use pocketmine\entity\Human;
+use pocketmine\item\Item;
+use pocketmine\level\Level;
+use pocketmine\level\Position;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\network\protocol\BlockEventPacket;
+use pocketmine\Player;
+
+class EnderChestInventory extends ContainerInventory{
+	 
+	private $owner;
+  
+	public function __construct(Human $owner, $contents = null){
+		$this->owner = $owner;
+		parent::__construct(new FakeBlockMenu($this, $owner), InventoryType::get(InventoryType::ENDER_CHEST));
+		if($contents !== null){
+			if($contents instanceof ListTag){
+ 			foreach($contents as $item){
+ 				$this->setItem($item["Slot"], Item::nbtDeserialize($item));
+ 			}
+      
+ 		}else{
+				throw new \InvalidArgumentException("Expecting ListTag, received " . gettype($contents));
+			}
+      
+		}
+    
+	}
+  
+	public function getOwner(){
+		return $this->owner;
+	}
+ 
+	public function openAt(Position $pos){
+		$this->getHolder()->setComponents($pos->x, $pos->y, $pos->z);
+		$this->getHolder()->setLevel($pos->getLevel());
+		$this->owner->addWindow($this);
+	}
+ 
+ 
+	public function getHolder(){
+		return $this->holder;
+	}
+  
+	public function onOpen(Player $who){
+		parent::onOpen($who);
+		if(count($this->getViewers()) === 1){
+			$pk = new BlockEventPacket();
+			$pk->x = $this->getHolder()->getX();
+			$pk->y = $this->getHolder()->getY();
+			$pk->z = $this->getHolder()->getZ();
+			$pk->case1 = 1;
+			$pk->case2 = 2;
+			if(($level = $this->getHolder()->getLevel()) instanceof Level){
+				$level->addChunkPacket($this->getHolder()->getX() >> 4, $this->getHolder()->getZ() >> 4, $pk);
+			}
+      
+		}
+    
+	}
+  
+	public function onClose(Player $who){
+		if(count($this->getViewers()) === 1){
+			$pk = new BlockEventPacket();
+			$pk->x = $this->getHolder()->getX();
+			$pk->y = $this->getHolder()->getY();
+			$pk->z = $this->getHolder()->getZ();
+			$pk->case1 = 1;
+			$pk->case2 = 0;
+			if(($level = $this->getHolder()->getLevel()) instanceof Level){
+				$level->addChunkPacket($this->getHolder()->getX() >> 4, $this->getHolder()->getZ() >> 4, $pk);
+			}
+      
+		}
+    
+		parent::onClose($who);
+	}
+  
+} 
